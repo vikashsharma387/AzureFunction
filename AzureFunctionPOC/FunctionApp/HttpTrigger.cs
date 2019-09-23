80b3b6024cfbd3b23d1c8149250eb6f1fe196ea5
vikash.sharma1@in.ey.com <Vikash.Sharma1@in.ey.com>
2019-09-23 13:47:09 +0530

    chnaged the return data

--- AzureFunctionPOC/FunctionApp/HttpTrigger.cs
+++ AzureFunctionPOC/FunctionApp/HttpTrigger.cs
@@ -27,7 +27,7 @@ namespace FunctionApp
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {name} {name}")
+                ? (ActionResult)new OkObjectResult($"Hello Welcome, {name} ")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
