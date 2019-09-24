25fae45b4086fc1675094c136ee19f56a6fd25ab
vikash.sharma1@in.ey.com <Vikash.Sharma1@in.ey.com>
2019-09-24 17:00:30 +0530

    Added Console App to test the third party API

---
+++ AzureFunctionPOC/FuncAppConsole/FuncAppConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FuncAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            InvokeMethod().Wait();
+            
+        }
+
+        public static async Task<HttpResponseMessage> InvokeMethod()
+        {
+            //Call API
+            HttpClient httpClient = new HttpClient();
+           // HttpRequestMessage objreq = new HttpRequestMessage(HttpMethod.Get, "http://dummy.restapiexample.com/api/v1/employees");
+
+            HttpResponseMessage httpResponseMessage = await httpClient.GetAsync("http://dummy.restapiexample.com/api/v1/employees");
+
+            string res = await httpResponseMessage.Content.ReadAsStringAsync();
+            var result = JsonConvert.DeserializeObject(res);
+
+            //HttpResponseMessage response = httpClient.GetAsync("api/Department/1");
+
+
+
+            return httpResponseMessage;
+        }
+    }
+}
